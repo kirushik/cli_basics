5dfdbb8b4af90465ebb15ae5bd45f44886e15f83
Kirill Pimenov <kirill@pimenov.cc>
2015-10-13 22:13:49 +0200

    Enabled CLI parsing and debugging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[macro_use]
+extern crate clap;
+
+#[macro_use]
+extern crate log;
+extern crate flexi_logger;
+
+use clap::{Arg, App, SubCommand, AppSettings};
+
 fn main() {
-    println!("Hello, world!");
+  let matches = App::new("cli_basics")
+                    .version("0.1")
+                    .author("Kirill Pimenov <kirill@pimenov.cc>")
+                    .about("Sample REST API client in Rust")
+                    .arg(Arg::with_name("debug")
+                             .short("d")
+                             .help("Enables debugging output"))
+                    .subcommand(SubCommand::with_name("get")
+                                           .help("Launch a GET request to a JSON endpoint"))
+                    .setting(AppSettings::SubcommandRequiredElseHelp)
+                    .get_matches();
+
+  if matches.is_present("debug") {
+    flexi_logger::init(flexi_logger::LogConfig::new(), Some("cli_basics=debug".to_string())).unwrap();
+  };
+
+  debug!("End of the app!");
 }
