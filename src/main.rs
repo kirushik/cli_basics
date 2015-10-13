df4aa7a786da90dc397e72b0aa97ce2dbc0b6c25
Kirill Pimenov <kirill@pimenov.cc>
2015-10-13 22:20:11 +0200

    Subcommand argument usage

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate clap;
 extern crate log;
 extern crate flexi_logger;
 
-use clap::{Arg, App, SubCommand, AppSettings};
+use clap::{Arg, App, SubCommand};
 
 fn main() {
   let matches = App::new("cli_basics")
@@ -17,12 +17,21 @@ fn main() {
                              .help("Enables debugging output"))
                     .subcommand(SubCommand::with_name("get")
                                            .help("Launch a GET request to a JSON endpoint"))
-                    .setting(AppSettings::SubcommandRequiredElseHelp)
+                    .subcommand_required_else_help(true)
                     .get_matches();
 
   if matches.is_present("debug") {
     flexi_logger::init(flexi_logger::LogConfig::new(), Some("cli_basics=debug".to_string())).unwrap();
   };
 
+  match matches.subcommand_name() {
+    Some("get") => run_get(),
+    _ => unreachable!()
+  };
+
   debug!("End of the app!");
+}
+
+fn run_get() {
+  debug!("Faking GET request");
 }
